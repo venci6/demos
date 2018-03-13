7d9af2f8398ce62d0fb11181ee4035a42ba1ef79
char <charlene.venci@gmail.com>
2018-03-13 17:00:30 -0400

    add inputs

--- wrap.sh
+++ wrap.sh
@@ -1,5 +1,9 @@
 MODEL=$1
 
+REPO=$2
+IMAGE=$3
+VERSION=$4
+
 until docker ps; 
 do sleep 3; 
 done; 
@@ -7,6 +11,6 @@ done;
 
 echo -e "FROM openjdk:8-jre\nADD model-serving.jar app.jar\nEXPOSE 8080\nENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/app.jar\",\"--s3.outputFile=${MODEL}\"]" >> Dockerfile
 
-docker build . -t pqchat/model-serving:v2
+docker build . -t ${REPO}/${IMAGE}:${VERSION}
 docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
-docker push pqchat/model-serving:v2
+docker push ${REPO}/${IMAGE}:${VERSION}
