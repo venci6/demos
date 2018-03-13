f8cc32bbbee9b146a6d0b4dff4861a5030cab226
char <charlene.venci@gmail.com>
2018-03-13 17:16:57 -0400

    fix

--- wrap.sh
+++ wrap.sh
@@ -4,6 +4,8 @@ REPO=$2
 IMAGE=$3
 VERSION=$4
 
+DOCKER_PATH = ${REPO}/${IMAGE}:${VERSION}
+
 until docker ps; 
 do sleep 3; 
 done; 
@@ -11,6 +13,7 @@ done;
 
 echo -e "FROM openjdk:8-jre\nADD model-serving.jar app.jar\nEXPOSE 8080\nENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/app.jar\",\"--s3.outputFile=${MODEL}\"]" >> Dockerfile
 
-docker build . -t ${REPO}/${IMAGE}:${VERSION}
+echo "Pushing to ${DOCKER_PATH}"
+docker build . -t DOCKER_PATH
 docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
 docker push ${REPO}/${IMAGE}:${VERSION}
