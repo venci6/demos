279b751a84d7e017095547288d0975113860b268
char <charlene.venci@gmail.com>
2018-03-13 16:45:32 -0400

    fix

--- wrap.sh
+++ wrap.sh
@@ -5,7 +5,7 @@ do sleep 3;
 done; 
 
 
-echo -e "FROM openjdk:8-jre\nADD model-serving.jar app.jar\nEXPOSE 8080\nENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/app.jar --s3.outputFile=${MODEL}\"]" >> Dockerfile
+echo -e "FROM openjdk:8-jre\nADD model-serving.jar app.jar\nEXPOSE 8080\nENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/app.jar\",\"--s3.outputFile=${MODEL}\"]" >> Dockerfile
 
 docker build . -t pqchat/model-serving:v2
 docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
