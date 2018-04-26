8f1eda5e592c970fbdcf79ec1c1ea8e49c6cf4d2
venci6 <char.venci@gmail.com>
2018-04-26 13:19:05 -0400

    Update wrap.sh

--- wrap.sh
+++ wrap.sh
@@ -14,9 +14,9 @@ do sleep 3;
 done; 
 
 
-echo -e "FROM openjdk:8-jre\nADD model-serving.jar app.jar\nEXPOSE 8080\nENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/app.jar\",\"--sentimentAnalysis.model=${MODEL}\",\"--s3.columns=${COLUMNS}\",\"--modelType=${TYPE}\"]" >> Dockerfile
+echo -e "FROM openjdk:8-jre\nADD app.jar app.jar\nEXPOSE 8080\nENTRYPOINT [\"java\",\"-Djava.security.egd=file:/dev/./urandom\",\"-jar\",\"/app.jar\",\"--sentimentAnalysis.model=${MODEL}\",\"--s3.columns=${COLUMNS}\",\"--modelType=${TYPE}\"]" >> Dockerfile
 
 echo "Pushing to ${DOCKER_PATH}"
 docker build . -t ${DOCKER_PATH}
 docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
-docker push ${REPO}/${IMAGE}:${VERSION}
+docker push ${DOCKER_PATH}
